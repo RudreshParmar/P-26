2a2bdba8c4a776f80d65cf8e000e6b7f659dff2f
RudreshParmar <75979481+RudreshParmar@users.noreply.github.com>
2021-01-23 12:21:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const body = Matter.Body;
+const Render= Matter.Render;
+const Constraint = Matter.Constraint;
+
+
+function setup() {
+	createCanvas(800, 400);
+
+	engine = Engine.create();
+	world = engine.world;
+
+bob1=new Bob(300,350,50);
+bob2=new Bob(350,350,50);
+bob3=new Bob(400,350,50);
+bob4=new Bob(450,350,50);
+bob5=new Bob(500,350,50);
+
+  roof = new Roof(400,100,300,40);
+  sling1= new String(bob1.body,roof.body,-50*2,0);
+  sling2= new String(bob2.body,roof.body,-25*2,0);
+  sling3= new String(bob3.body,roof.body,-0*2,0);
+  sling4= new String(bob4.body,roof.body,25*2,0);
+  sling5= new String(bob5.body,roof.body,50*2,0);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background("lightyellow");
+
+  rectMode(CENTER);
+  Engine.update(engine);
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  
+  roof.display();
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  sling5.display();
+
+  keyPressed();
+  drawSprites ();
+}
+
+function keyPressed() { 
+	if (keyCode === UP_ARROW) 
+	{ Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-100,y:-100}); } 
+} 
